95c0cc4f088313f650e74a706907e4dd969db2d9
Peter Nieuwenhuyse <peter.nieuwenhuyse@gmail.com>
2013-12-20 13:42:03 +0100

    ships placement ok

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,7 +1,10 @@
 
 var roomInfra = io.connect('/room_infra');
 var gameCom = io.connect('/game_com');
-
+var positions_taken=[];
+var ship_chosen ="";
+var size = 0;
+var orientation_hor = true;
 $(function(){
   var username="";
   
@@ -83,8 +86,105 @@ var firstcel = $('<div class="cell"></div>var');
   //het zetten van de schepen voorlopige versie nog geen controle op beschikbare plaats en correcte setup schepen  
   $('.cell').on('click',function(event){
    event.preventDefault();
-   $(this).toggleClass("free_field chosen");
- });
+   if(ship_chosen !=="" && size !== 0){
+    if(orientation_hor !==false){
+      $el =$(this);
+      var free_field=true;
+      for (var i = 0 ; i<size; i++){  
+        var check = (parseInt($el.attr('itemprop'))%10)+i;
+        console.log(check);
+        if(check >9){
+          free_field=false;
+          alert("ship can't be placed here");
+        }
+        for (var j=0; j<positions_taken.length; j++){
+          for (var k=0; k<size;k++){
+            if(free_field!=false){
+            if((parseInt($el.attr('itemprop'))+k) == positions_taken[j]){
+              free_field=false;
+              alert('this would cause overlapsing sir)');
+            }
+            }
+          }
+        }
+        }
+      if(free_field != false){
+      for (var i = 0 ; i<size ;i++){
+        $el.toggleClass("free_field chosen");
+        positions_taken.push($el.attr('itemprop'));
+        $el= $el.next();
+      }
+      
+      ship_chosen = "";
+      size=0;
+      }
+    }else{
+      $el =$(this);
+      var free_field=true;
+      for (var i=0; i<size;i++){
+        if((parseInt($el.attr("itemprop"))+(i*10))>100){
+          free_field=false;
+        }
+        for (var j=0; j<positions_taken.length; j++){
+          for (var k=0; k<size;k++){
+            if(free_field!=false){
+            if((parseInt($el.attr('itemprop')))+(k*10) == positions_taken[j]){
+              free_field=false;
+              alert('this would cause overlapsing sir)');
+            }
+            }
+          }
+        }
+      }
+      if(free_field!=false){
+        for(var i=0;i<size;i++){
+          var propvalue= parseInt($(this).attr('itemprop'))+(i*10)
+          console.log(propvalue);
+          $('.free_field').each(function(){
+            if($(this).attr('itemprop')==propvalue){
+              $(this).toggleClass('free_field chosen');
+              positions_taken.push($(this).attr('itemprop'));
+              console.log(positions_taken);
+            }
+          });
+        }
+        ship_chosen  ="";
+        size=0
+      }
+    }
+   
+ }
+   });
+  //
+   var ship_prop =[
+     {name:"Carrier",size:5, placed:0},
+     {name:"Battleship",size:4, placed:0},
+     {name:"Cruiser",size:3, placed:0},
+     {name:"Submarine",size:3, placed:0},
+     {name:"Destroyer",size:2 ,placed:0}
+   ];   
+   $('.ship').on('click',function(e){
+     e.preventDefault();
+     ship_chosen = $(this).attr('id');
+     for(var i=0 ;i<ship_prop.length; i++){
+       if(ship_chosen===ship_prop[i].name){
+         if(ship_prop[i].placed !==1){
+         size= ship_prop[i].size;
+         ship_prop[i].placed=1;
+       } else {
+           alert('this ship is already taken commander please select a other ship')
+         }
+       }
+     }
+   });
+   $('.horizontal').on('click',function(){
+     orientation_hor = true;
+   });
+   $('.vertical').on('click', function(){
+     orientation_hor = false;
+   });
+   
+ 
 });
 
 

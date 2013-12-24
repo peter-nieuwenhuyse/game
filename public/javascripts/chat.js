d49e935edc072ee74d85e51f5759cc5982f18b98
unknown <peter.nieuwenhuyse@gmail.com>
2013-12-24 13:53:28 +0100

    fixed chat errors

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -10,23 +10,26 @@ $(function(){
           var el= $('<div class="own_message"></div>');
           el.html(username + ': ' + message);
           $('#messages').append(el);
+          $("#messages").scrollTop($("#messages")[0].scrollHeight+16);
           chatInfra.emit('send_message', username, message);
           $('#message').val('');
           console.log(message);
           } else{
              var el=$('<div class ="systemmessage"></div>');
-             var message= 'username is required to chat please set a username first';
+             var message= 'username is required to chat please set a username';
              el.html(message);
-             $('#messages').append(message);
+             $('#messages').append(el);
+             $("#messages").scrollTop($("#messages")[0].scrollHeight);
           }
         };   
       });
       chatInfra.on('message_recieved',function(user,message){
           var el = $('<div class="recieved_message"></div>');
           var message= '<span class="user">' + user +': '+ message +'</span>';
-          el.html(message)
+          el.html(message);
           $('#messages').append(el);
-      })
+          $("#messages").scrollTop($("#messages")[0].scrollHeight);
+      });
 });
 
 
